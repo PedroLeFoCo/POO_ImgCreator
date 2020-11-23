a1d00ea97b72c12b8bc4693deea9bf4ce6d11404
sd-Hector <sidh.96@outlook.com>
2020-11-23 14:23:27 +0100

    Ajout de class Figure

--- src/Figure.cpp
+++ src/Figure.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 #include "Figure.h"
 
+// Lorsqu'on déclare une classe dérivée, on utilise la syntaxe suivante : 
+class Figure : public Drawing {
+    public:
+        Figure()
+
+        Chien(std::string nom) : Animal(nom) {
+            nombreDePatte = 4;
+            std::cout << "Je suis un chien" << std::endl;
+            afficherNombrePatte();
+    }
+};
 
 void carre(Drawing MonImage, int size, int color)
 {
