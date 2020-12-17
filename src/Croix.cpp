45250cfe4b216a0c8a31563e2acc4b94efe41fd3
sd-Hector <sidh.96@outlook.com>
2020-12-17 20:59:48 +0100

    Croix fonctionnelle

--- src/Croix.cpp
+++ src/Croix.cpp
@@ -2,27 +2,25 @@
 
 Croix::Croix(int width, int height)
 {
-    std::cout<<"In Croix Constructor"<<std::endl;
     m_CroixWidth = width;
     m_CroixHeight = height;
 
     m_FigureImg.resize(m_CroixHeight * m_CroixWidth);
-    std::cout<<"After Resize"<<std::endl;
 }
 
 void Croix::tracerCroix()
 {
-    //Tracer la croix
-    //On trace le trait horizontal de la croix
-    for(int i = 0; i<m_CroixWidth; i++)
-    {
-        int numCharColorWhite = i * m_CroixWidth + m_CroixHeight/2;
-        setPointColorWhite(numCharColorWhite);
-    }
-    //On trace le trait vertical de la croix
+    //On trace le trait vertical 
     for(int i = 0; i<m_CroixHeight; i++)
     {
-        int numCharColorWhite = i + m_CroixHeight/2;
+        int numCharColorWhite = (i*m_CroixWidth) + m_CroixWidth/2 ;
         setPointColorWhite(numCharColorWhite);
     }
+    //On trace le trait horizontal
+    for(int i = 0; i<m_CroixWidth; i++)
+    {
+        int numCharColorWhite = i + (m_CroixHeight/2)*m_CroixWidth;
+        setPointColorWhite(numCharColorWhite);
+    }
+
 }
