a8fa42ecd92e2b12f2ac4d746381272750509333
Benji-de-Marseille <benjamin.bortolotti@isen.yncrea.fr>
2020-12-17 22:24:12 +0100

    ajout de cercle mais ca buuuugg...

---
+++ src/Cercle.h
@@ -0,0 +1,32 @@
+#ifndef CERCLE_H
+#define CERCLE_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include "Figure.h"
+
+// class figure
+class Cercle: public Figure {
+public :
+	Cercle();     // Constructeur
+    Cercle(int centreX, int centreY, int rayon);
+	~Cercle()     // Destructeur
+    {
+        std::cout << "Deleting Figure " << Name << std::endl;
+    }
+
+// methode
+    int getCercleCentreX ()  
+    int getCercleCentrey ()  
+    int getCercleRayon ()
+    std::string Name;
+    void tracerCercle(int CentreX, int CentreY);
+    
+private :
+	int centreX;
+    int centreY;
+    int rayon;
+};
+
+#endif /* CERCLE_H */
